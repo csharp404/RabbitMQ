3a9f5b0e124d5cbe9bde8d960b88eaab632e9af3
csharp404 <yousefjobsdeve@gmail.com>
2024-10-29 05:46:13 -0700

    docker RabbitMQ

---
+++ RabbitReceiver/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        ConnectionFactory client = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+
+        using (IConnection cnn = client.CreateConnection())
+        using (IModel channel = cnn.CreateModel())
+        {
+            string exchangeName = "myName";
+            string routingKey = "Route-My-Key";
+            string queueName = "MyQueue";
+
+            
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+
+            
+            channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            
+            channel.QueueBind(queueName, exchangeName, routingKey);
+
+            
+            channel.BasicQos(0, 1, false);
+
+           
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(0.5)).Wait();
+                var msg = args.Body.ToArray();
+                var pureMsg = Encoding.UTF8.GetString(msg);
+                Console.WriteLine($"My message is: {pureMsg}");
+
+               
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            
+            string consTag = channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+
+            
+            channel.BasicCancel(consTag);
+        } 
+    }
+}
---
+++ RabbitServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        ConnectionFactory client = new ConnectionFactory();
+        client.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        using (IConnection cnn = client.CreateConnection())
+        using (IModel channel = cnn.CreateModel())
+        {
+            string exchangeName = "myName";
+            string routingKey = "Route-My-Key";
+            string queueName = "MyQueue";
+
+            // Declare the exchange
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+
+            // Declare the queue
+            channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            // Bind the queue to the exchange
+            channel.QueueBind(queueName, exchangeName, routingKey);
+
+            // Create a message and publish it
+            for (int i = 0; i < 10; i++) {
+                Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+                byte[] message = Encoding.UTF8.GetBytes("HELLO WORLD");
+                channel.BasicPublish(exchangeName, routingKey, null, message);
+            }
+        } // Channels and connections will be closed automatically here
+    }
+}
